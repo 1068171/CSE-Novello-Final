267dd33a5518fab1ae562504e6b0578dac659723
JackTellers <1067472@lammersvilleusd.net>
2016-05-19 13:50:55 -0700

    John Changes
    Added Name_to_number function

--- __main__.py
+++ __main__.py
@@ -1,2 +1,10 @@
+# -*- coding: utf-8 -*-
 #Before you push your additions to the code, make sure they exactly fit the parameters described in the project!
 #Let's write delicious code. がんばってくらさい！
+
+#John Philip Earl Allosa ( ͡° ͜ʖ ͡°) 5/19/16
+def Name_to_number(theName):
+   parsedName = Name_parse(theName)
+   genNumber = Parse_to_number(parsedName)
+   return genNumber  
+   
