ec9157b8b8b43e78a21fb6fdf33074985999a509
tofuu <1069932@lammersvilleusd.net>
2016-05-23 13:41:02 -0700

    lpf
    ey lmao

--- __main__.py
+++ __main__.py
@@ -4,16 +4,27 @@
 
 
 # Abirami
+import math
+
 def list_prime_factors(num):
     n = int(num)
     prime = []
-    for x in range (2,n+1):
-        pr = True
-        for i in range (2,x):
-            if (x%i==0):
-                pr = False  
-        if pr:
-            if n%x==0:
-                prime.append(x)
+    k=1
+    if n%2==0:
+        prime.append(2)
+        
+    flag = 0
+    while (2*k+2)**2 < n:
+        if n%(2*k+1)==0:
+            for p in prime:
+                if (2*k+1)%p ==0:
+                    flag = 1
+            if flag ==+ 0:
+                prime.append(2*k+1)
+        flag = 0
+        k=k+1
+    if len(prime)== 0:
+        prime.append(n)
+        
     print prime
     return prime
