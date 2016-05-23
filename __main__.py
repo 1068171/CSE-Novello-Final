1921f9b826e8bac203098042fed24e3bd04b0c71
jgutierrez16 <1071157@lammersvilleusd.net>
2016-05-23 13:12:04 -0700

    Revert "created the list_of_pairs function"
    This reverts commit 5a054e68fde6a13fbd6b7c5b021a6d608b9ffae6.

--- __main__.py
+++ __main__.py
@@ -1,13 +1,2 @@
-# -*- coding: utf-8 -*-
 #Before you push your additions to the code, make sure they exactly fit the parameters described in the project!
 #Let's write delicious code. がんばってくらさい！
-
-#jonathan's code
-
-def list_of_pairs (number):
-    a_list = []
-    list_of_factors = List_prime_factors (number)
-    for factor in list_of_factors:
-        multiplicity = Find_Multiplicity (factor, number)
-        a_list.append([ factor, multiplicity ])
-    return a_list
