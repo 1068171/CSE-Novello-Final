6fc381feadae77a52ad3d075fb095c17e416d223
AchyuthVarma <10620578@lammersvilleusd.net>
2016-05-23 13:03:36 -0700

    parse_to_number
    cleaning up, removing useless functions

--- Love Tester.py
+++ Love Tester.py
@@ -1,57 +1,3 @@
-def alpha_numeric_dictionary(letter):
-    if letter == 'a':
-        return 1
-    if letter == 'b':
-        return 2    
-    if letter == 'c':
-        return 3
-    if letter == 'd':
-        return 4
-    if letter == 'e':
-        return 5
-    if letter == 'f':
-        return 6    
-    if letter == 'g':
-        return 7
-    if letter == 'h':
-        return 8
-    if letter == 'i':
-        return 9
-    if letter == 'j':
-        return 10    
-    if letter == 'k':
-        return 11
-    if letter == 'l':
-        return 12
-    if letter == 'm':
-        return 13
-    if letter == 'n':
-        return 14   
-    if letter == 'o':
-        return 15
-    if letter == 'p':
-        return 16
-    if letter == 'q':
-        return 17
-    if letter == 'r':
-        return 18   
-    if letter == 's':
-        return 19
-    if letter == 't':
-        return 20
-    if letter == 'u':
-        return 21
-    if letter == 'v':
-        return 22    
-    if letter == 'w':
-        return 23
-    if letter == 'x':
-        return 24
-    if letter == 'y':
-        return 25
-    if letter == 'z':
-        return 26    
-        
 def parse_to_number(achyuth):
     final = 0
     letters = []
@@ -67,6 +13,5 @@ def parse_to_number(achyuth):
     for i in range(0, len(letters)):
         numero = alpha_numeric_dictionary(letters[i])
         final = final + (numero * (26**(numbers[i]%9)))
-    print final
+    return final
 
-parse_to_number((('p', 1), ('a', 2), ('t', 3), ('r', 4), ('i', 5), ('c', 6), ('k', 7), ('s', 8), ('u', 9), ('c', 9), ('k', 10), ('s', 11)))
