6d84022b67553bd52f298ea7891b0fd5625dd54c
Bryant Hoang <10650254@lammersvilleusd.net>
2016-05-23 13:19:07 -0700

    Multiplicity Function
    Takes in a number and a prime factor of that number and returns the
    multiplicity of the factor.

--- __main__.py
+++ __main__.py
@@ -1,2 +1,13 @@
+# -*- coding: utf-8 -*-
 #Before you push your additions to the code, make sure they exactly fit the parameters described in the project!
 #Let's write delicious code. がんばってくらさい！
+
+#Alec Battisti Multiplicity Function
+def multiplicity(f,n):
+    mult = 0
+    while n%f == 0:
+        mult+=1
+        n = n/f
+    print "Multiplicity: "+str(mult)
+    return mult
+    
