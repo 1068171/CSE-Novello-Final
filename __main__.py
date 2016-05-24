db74d10fb0ead098cac39d157a8fa1f66b735f7b
1068171 <1068171@lammersvilleusd.net>
2016-05-24 11:19:29 -0700

    Steve Manual

--- __main__.py
+++ __main__.py
@@ -352,3 +352,21 @@ def list_prime_factors(num):
         
     print prime
     return prime
+
+print "Enter in loveTest in the commandline followed by the two names in parentheses with commas in between"
+def loveTest(name1, name2)
+    number1 = name_to_number(name1)
+    number2 = name_to_number(name2)
+    try:
+        number1 = int(number1)
+        number2 = int(number2)
+    except:
+        print("Error with name_to_number function. Attempted to make the output of the function into an integer but failed")
+    factor1 = factor(number1)
+    factor2 = factor(number2)
+    try:
+        comparedFactor1 = compare_factor(factor1, factor2)
+    except:
+        print("Error in comparedFactor1 function")
+    InterestingResponse1 = InterestingResponseFunction(comparedFactor1)
+    print InterestingResponse1
